027ad6e86686bc05737b7d6675b5c72d13632f01
Skylar Robinson <skylardawn150@gmail.com>
2024-09-27 16:44:19 -0700

    Commit 2

--- lab15.cpp
+++ lab15.cpp
@@ -8,6 +8,8 @@ This program demonstrates a movie class object
 
 #include <iostream>
 #include <string>
+#include <fstream>
+#include <vector>
 using namespace std;
 
 class Movie{
@@ -24,10 +26,39 @@ public:
 	string getTitle() const  { return title; }
 	string getWriter() const { return writer; }
 	int getYear() const      { return year; }
+
+	//print
+	void print(){
+		cout << "Title: " << title << endl;
+		cout << "Writer: " << writer << endl;
+		cout << "Year: " << year << endl;
+	}
 };
 
 int main(){
+	//declare container
+	vector<Movie> movies;
 
+	//declare temp variables
+	string n, w;
+	int y;
+	Movie temp;
+
+	//open input file
+	ifstream in;
+	in.open("input.txt");
+	if (!in){
+		cout << "File open error.\n";
+		return -1;
+	}
+
+	//populate movies vector
+	while (!in.eof()){
+
+	}
+
+	//close file
+	in.close();
 
 	return 0;
 }
